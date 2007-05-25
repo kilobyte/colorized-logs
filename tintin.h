64315e131803524220dcb1122d8a8b4079c13da2
Adam Borowski <kilobyte@angband.pl>
2007-05-25 10:22:35 +0000

    off-by-one error in isw2width()

--- tintin.h
+++ tintin.h
@@ -354,11 +354,11 @@ struct ttyrec_header
 /* Japanese/Chinese double-width chars.  We can't use wcwidth() as that's
    a GNU extension.  The code below is buggy as it should return 0 for
    non-printables, but nyah... */
-#define isw2width(x) ((x)>=0x1100  && ((x)<0x11ff ||	\
-                      (x)>=0x2e80) && ((x)<0xd7ff ||	\
-                      (x)>=0xf900) && ((x)<0xfaff ||	\
-                      (x)>=0xfe30) && ((x)<0xfe6f ||	\
-                      (x)>=0xff01) && ((x)<0xff60 ||	\
-                      (x)>=0xffe0) && ((x)<0xffe6 ||	\
-                      (x)>=0x20000) && (x)<0x2ffff)
+#define isw2width(x) ((x)>=0x1100  && ((x)<=0x11ff ||	\
+                      (x)>=0x2e80) && ((x)<=0xd7ff ||	\
+                      (x)>=0xf900) && ((x)<=0xfaff ||	\
+                      (x)>=0xfe30) && ((x)<=0xfe6f ||	\
+                      (x)>=0xff01) && ((x)<=0xff60 ||	\
+                      (x)>=0xffe0) && ((x)<=0xffe6 ||	\
+                      (x)>=0x20000) && (x)<=0x2ffff)
 #define EMPTY_CHAR 0xffff
