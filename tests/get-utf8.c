b7ecf0eca911a7c180739ce850ae07cd3fc8a998
Adam Borowski <kilobyte@angband.pl>
2015-07-21 08:06:51 +0200

    Smart locale detection for the testsuite.
    We need any UTF-8 locale, but neither C.UTF-8 nor en_US.UTF-8 are guaranteed,
    and our old check did not work at least on FreeBSD and Solaris.

---
+++ tests/get-utf8.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+#include <locale.h>
+#include <langinfo.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <errno.h>
+
+static void sl(const char* loc)
+{
+    if (!setlocale(LC_CTYPE, loc))
+        return;
+    if (strcmp(nl_langinfo(CODESET), "UTF-8"))
+        return;
+    printf("%s\n", loc);
+    exit(0);
+}
+
+int main()
+{
+    /* try preferred values first */
+    sl("C.UTF-8");
+    sl("en_US.UTF-8");
+
+    DIR* dir = opendir("/usr/share/locale");
+    /* In that dir, FreeBSD and Solaris have qualified locale names.  OpenBSD
+       has bare "UTF-8" which does work.  Linux has unqualified dirs whose
+       names form ancient locales (except for some, like "vi"), but on the
+       other hand, Linux is likely to have "C.UTF-8".
+    */
+    if (!dir)
+    {
+        fprintf(stderr, "Can't read /usr/share/locale: %s\n", strerror(errno));
+        return 1;
+    }
+    struct dirent *d;
+    while ((d = readdir(dir)))
+        sl(d->d_name);
+    return 1;
+}
--- tests/runtest.sh
+++ tests/runtest.sh
@@ -3,10 +3,7 @@
 echo "#mess all 0;#delay 5 {#showme TIMEOUT;#end}" >_be_quiet_
 rm -rf aa
 
-if [ -z "`LC_ALL=C.UTF-8 locale 2>&1 >/dev/null`" ]
-  then LC_ALL=C.UTF-8
-  else LC_ALL=en_US.UTF-8
-fi
+LC_ALL=`get-utf8`
 LC_CTYPE=$LC_ALL
 export LC_ALL
 export LC_CTYPE
