70d77c7fb07538f6cce5d0abd56e11136c736fef
Adam Borowski <kilobyte@angband.pl>
2016-06-26 13:16:07 +0200

    ansi2txt: support non-ASCII, \e], \e%.

--- ansi2txt.c
+++ ansi2txt.c
@@ -5,14 +5,24 @@ int main()
     int ch;
     do
     {
-        if ((ch=getchar())=='\n')
-            putchar('\n');
-        else
-        if ((ch>=32)&&(ch<127))
+        if ((ch=getchar())==13)
+            ;
+        else if (ch==27)
+            if ((ch=getchar())=='[')
+                while ((ch=getchar())==';'||(ch>='0'&&ch<='9')||ch=='?');
+            else if (ch==']'&&(ch=getchar())>=0&&ch<='9')
+                for (;;)
+                {
+                    if ((ch=getchar())==EOF||ch==7)
+                        break;
+                    else if (ch==27)
+                        {ch=getchar(); break;}
+                }
+            else if (ch=='%')
+                ch=getchar();
+            else {}
+        else if (ch!=EOF)
             putchar(ch);
-        else
-        if (ch==27)
-            while ((ch=getchar())=='['||ch==','||(ch>='0'&&ch<='9')||ch==';'||ch=='?');
     }
     while (ch!=EOF);
     return 0;
