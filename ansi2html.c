4d6068f3078b8024bd4e817aade4d28db14a11cb
Adam Borowski <kilobyte@angband.pl>
2016-07-13 18:35:45 +0200

    Fix indentation.

--- ansi2html.c
+++ ansi2html.c
@@ -24,28 +24,28 @@ struct rgb { u8 r; u8 g; u8 b; };
 
 struct rgb rgb_from_256(int i)
 {
-   struct rgb c;
-   if (i < 8)
-   {   /* Standard colours. */
-       c.r = i&1 ? 0xaa : 0x00;
-       c.g = i&2 ? 0xaa : 0x00;
-       c.b = i&4 ? 0xaa : 0x00;
-   }
-   else if (i < 16)
-   {
-       c.r = i&1 ? 0xff : 0x55;
-       c.g = i&2 ? 0xff : 0x55;
-       c.b = i&4 ? 0xff : 0x55;
-   }
-   else if (i < 232)
-   {   /* 6x6x6 colour cube. */
-       c.r = (i - 16) / 36 * 85 / 2;
-       c.g = (i - 16) / 6 % 6 * 85 / 2;
-       c.b = (i - 16) % 6 * 85 / 2;
-   }
-   else/* Grayscale ramp. */
-      c.r = c.g = c.b = i * 10 - 2312;
-   return c;
+    struct rgb c;
+    if (i < 8)
+    {   /* Standard colours. */
+        c.r = i&1 ? 0xaa : 0x00;
+        c.g = i&2 ? 0xaa : 0x00;
+        c.b = i&4 ? 0xaa : 0x00;
+    }
+    else if (i < 16)
+    {
+        c.r = i&1 ? 0xff : 0x55;
+        c.g = i&2 ? 0xff : 0x55;
+        c.b = i&4 ? 0xff : 0x55;
+    }
+    else if (i < 232)
+    {   /* 6x6x6 colour cube. */
+        c.r = (i - 16) / 36 * 85 / 2;
+        c.g = (i - 16) / 6 % 6 * 85 / 2;
+        c.b = (i - 16) % 6 * 85 / 2;
+    }
+    else/* Grayscale ramp. */
+        c.r = c.g = c.b = i * 10 - 2312;
+    return c;
 }
 
 
