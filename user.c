f8c6903e961c03ddfc388dd3eb30896ce2a3bac0
Adam Borowski <kilobyte@angband.pl>
2016-09-05 14:45:37 +0200

    Drop a redundant and unused #define.

--- user.c
+++ user.c
@@ -3,10 +3,6 @@
 #include "protos/user_tty.h"
 #include "protos/utils.h"
 
-extern int colors[];
-extern char *attribs[];
-#define COLORCODE(c) "\033[0%s;3%d;4%d%sm",((c)&8)?";1":"",colors[(c)&7],colors[((c)>>4)&7],attribs[(c)>>7]
-
 char done_input[BUFFER_SIZE];
 bool tty;
 bool isstatus;
