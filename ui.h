fb9f6a0160c06eee5b3bac4adf3a6e36f15d8485
Adam Borowski <kilobyte@angband.pl>
2016-09-05 15:24:22 +0200

    Gather extern variables in protos like it's done for functions.

--- ui.h
+++ ui.h
@@ -11,8 +11,6 @@ bool tty;
 char done_input[BUFFER_SIZE];
 int color, lastcolor;
 
-extern int colors[];
-extern const char *attribs[16];
 #define COLORCODE(c) "\033[0%s;3%d;4%d%sm",((c)&8)?";1":"",colors[(c)&7],colors[((c)>>4)&7],attribs[(c)>>7]
 
 typedef void (voidfunc)(void);
