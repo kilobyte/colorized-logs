0d35ee486525c8bd316d9e4ea27fae269947883d
Adam Borowski <kilobyte@angband.pl>
2017-08-04 22:18:17 +0200

    Drop support for systems without openpty().
    Goodbye IRIX and co -- this code hasn't been tested for two decades, so
    chances it still works are minimal.

--- acinclude.m4
+++ acinclude.m4
@@ -1,3 +1,1 @@
-m4_include([ac_ptyranges.m4])
 m4_include([ac_sco.m4])
-m4_include([ac_c_try_flag.m4])
--- pty.h
+++
@@ -1,3 +0,0 @@
-extern int openpty(int *amaster, int *aslave, char *dummy, struct termios *termp, struct winsize *wp);
-extern int forkpty(int *amaster, char *dummy, struct termios *termp, struct winsize *wp);
-extern void cfmakeraw(struct termios *ta);
