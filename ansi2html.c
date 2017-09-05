kilobyte/colorized-logs

commit 9bbc5368bad4474b3700b57e58253ab96c4b38b6

ansi2html: make a few variables local.

ansi2html.c
@@ -16,9 +16,6 @@ static int fg, bg, fl, frgb, brgb;
 static const char *cols[]={"BLK","RED","GRN","YEL","BLU","MAG","CYN","WHI",
                            "HIK","HIR","HIG","HIY","HIB","HIM","HIC","HIW"};
 
-static int ntok, tok[10];
-static int ch;
-
 typedef unsigned char u8;
 
 
@@ -253,7 +250,8 @@ int main(int argc, const char **argv)
     fl=0;
     in_span=false;
     frgb=brgb=-1;
-    ch=getchar();
+    char ch=getchar();
+    int ntok, tok[10];
 normal:
     switch (ch)
     {

commit 317580cc3f34d524fb8a97ed76354ff1cd8b60d1

Fix bad handling of EOF.
On signed-char architectures, 0xFF bytes stopped processing.  On
unsigned-char, there was an infinite loop.

Was broken since 9bbc5368bad4474b3700b57e58253ab96c4b38b6 (not in any
release).

ansi2html.c
@@ -286,7 +286,7 @@ int main(int argc, const char **argv)
     fl=0;
     in_span=false;
     frgb=brgb=-1;
-    char ch=getchar();
+    int ch=getchar();
     unsigned int ntok, tok[16];
 normal:
     switch (ch)